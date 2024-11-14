8cb74b3aea279d5eb0118c5e9ee2a103970c42fe
hydrartew <hydrartew@mail.ru>
2024-11-14 15:51:45 +0300

    изменена схема Prediction

--- schemas.py
+++ schemas.py
@@ -7,30 +7,13 @@ from requests import Response
 from requests.structures import CaseInsensitiveDict
 
 
-# class YandexPassportData(BaseModel):
-#     model_config = ConfigDict(arbitrary_types_allowed=True)
-#
-#     headers: CaseInsensitiveDict | dict
-#     time_created: datetime
-#     csrf_tokens: CsrfTokens = CsrfTokens()
-#
-#     @field_validator('headers')
-#     @classmethod
-#     def validate_headers(cls, v: CaseInsensitiveDict) -> CaseInsensitiveDict:
-#         v['User-Agent'] = 'SuptechTaxi script by @amayammi'
-#         return v
-#
-#     @computed_field
-#     @property
-#     def headers_expired(self) -> bool:
-#         return (datetime.now() - self.time_created).total_seconds() > settings.yt_session_expired
-
 class Prediction(BaseModel):
+    prediction_id: int
     accepted: bool = True
-    author_tg_user_id: int
-    author_staff_login: str
+    author_tg_user_id: int = 0
+    author_staff_login: str = 'admin'
     dttm_created: datetime
-    dttm_last_usage: datetime | None = None
+    dttm_last_usage: datetime = datetime(1970, 1, 1)
     text: str
 
     @field_validator('text')
