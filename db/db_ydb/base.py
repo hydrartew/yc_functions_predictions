669361a5aec07e72eea0efd187883077a8b481dc
hydrartew <hydrartew@mail.ru>
2024-11-14 16:12:39 +0300

    remove file base.py

--- db/db_ydb/base.py
+++
@@ -1,8 +0,0 @@
-import ydb.iam
-from config_data import credentials, settings
-
-driver = ydb.Driver(
-    endpoint=settings.YDB_ENDPOINT,
-    database=settings.YDB_DATABASE,
-    credentials=credentials,
-)
